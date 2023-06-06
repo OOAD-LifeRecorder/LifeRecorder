458b0060e05611fd085cd94727f12812fff01315
Chan Jiewen <jiewen1010@gmail.com>
2023-06-06 15:58:36 +0800

    fix: bug

--- MainApp.py
+++ MainApp.py
@@ -95,15 +95,7 @@ class ItemDrawer(OneLineIconListItem):
     text_color = ListProperty((0, 0, 0, 1))
 
 class DrawerList(MDList):
-    def set_color_item(self, instance_item):
-        """Called when tap on a menu item."""
-
-        # Set the color of the icon and text for the menu item.
-        for item in self.children:
-            if item.text_color == self.theme_cls.primary_color:
-                item.text_color = self.theme_cls.text_color
-                break
-        instance_item.text_color = self.theme_cls.primary_color
+    pass
 
 class MainApp(MDApp):
     def build(self):
